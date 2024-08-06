f760a76e712565f663b6ae849a38b1d66745a101
princessubandoma <princessubandoma396@gmail.com>
2024-08-06 15:24:05 +0200

    remove about me and contact me pages

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello There!</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Princess</p>");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +491762223646</p>");
-});
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
